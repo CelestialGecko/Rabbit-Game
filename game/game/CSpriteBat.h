db2cacfb9f202efe2f7ae42502689fd1b2915f8e
Jamie Macdonald <K2404297@kingston.ac.uk>
2025-03-17 10:12:48 +0000

    Bat progress 1

--- game/game/CSpriteBat.h
+++ game/game/CSpriteBat.h
@@ -1,12 +1,18 @@
 #pragma once
 // for the bat sprites to give them more properties
+enum stateAni { SLEEP, TAKEOFFL, TakeOFFR, FLYL, FLYR, DIE, DEAD };
 class CSpriteBat :
     public CSprite
 {
 private:
-    char state;
+    stateAni s;
 public:
     CSpriteBat(CRectangle r, Uint32 time);
+    void LoadAnimations(char* idle, char* fly, char* off, char* die);
+    void BetterHitTest(CSprite& s);
+    stateAni GetStateAni() const { return s; }
+    void KillBat() { s = DIE; }
+    void PlayerDetected();
 protected:
     virtual void OnDraw(CGraphics* g);
 };
