a74cc3a18d53528a8b744899ce4ef76f70081c18
Geck GD <129905932+CelestialGecko@users.noreply.github.com>
2025-04-13 13:06:12 +0100

    Update stdafx.h
    try 4

--- game/game/stdafx.h
+++ game/game/stdafx.h
@@ -15,7 +15,7 @@
 #define max(x, y)	((x) > (y) ? (x) : (y))
 #define min(x, y)	((x) < (y) ? (x) : (y))
 
-#include "GFC.h"
+#include "gfc/gfc/include/GFC.h"
 #include "CSpriteBat.h"
 #include "CSpriteWorm.h"
 using namespace std;
